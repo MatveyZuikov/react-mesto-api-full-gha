c930711e14f830ff8c482d82201e3a12ba982c89
Matvey Zuikov <zuikov.matvei@gmail.com>
2023-11-19 01:43:12 +0300

    15.5

--- backend/app.js
+++ backend/app.js
@@ -54,6 +54,12 @@ app.get("/", (req, res) => {
   res.send("hello");
 });
 
+app.get("/crash-test", () => {
+  setTimeout(() => {
+    throw new Error("Сервер сейчас упадёт");
+  }, 0);
+});
+
 app.post(
   "/signin",
   celebrate({
